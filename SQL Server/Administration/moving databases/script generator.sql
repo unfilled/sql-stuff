ae542bcb7bd45340aaa2e18706b18d54a8c8a70f
oleg aronov <aronov.oleg@gmail.com>
2020-02-15 18:29:15 +0600

    исправлена опечатка

--- SQL Server/Administration/moving databases/script generator.sql
+++ SQL Server/Administration/moving databases/script generator.sql
@@ -3,7 +3,7 @@
 	, @new_data_path as varchar(max) = 'D:\SQLServer\data\';
 
 SELECT name	
-	, 'BACKUP DATABASE [' + name + '] TO DISK = ''' + @unc_backup_path + name + '.bak'' WITH STATS = 5;' AS backup_command
+	, 'BACKUP DATABASE [' + name + '] TO DISK = ''' + @unc_backup_path + name + '.bak'' WITH INIT, STATS = 5;' AS backup_command
 	, 'ALTER DATABASE [' + name + '] SET OFFLINE WITH ROLLBACK IMMEDIATE;' AS offline_command
 	, 'RESTORE DATABASE [' + name + '] FROM DISK = ''' + @local_backup_path + name + '.bak'' WITH ' 
 		+ (
@@ -13,7 +13,7 @@ SELECT name
 			FROM sys.master_files mf
 			WHERE mf.database_id = d.database_id
 			FOR XML PATH('')
-		) + 'INTI, REPLACE, RECOVERY, STATS = 5;' AS restore_command
+		) + 'REPLACE, RECOVERY, STATS = 5;' AS restore_command
 FROM sys.databases d
 WHERE database_id > 4 AND state_desc = N'ONLINE';
 
